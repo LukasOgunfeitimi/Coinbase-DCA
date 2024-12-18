f98f7c1bbc8c1cf0760fbed726486a35f86901f2
lukas <lukas7865@yahoo.co.uk>
2024-12-18 18:01:01 +0000

    .

--- operations/DCA.js
+++ operations/DCA.js
@@ -11,13 +11,13 @@ const { order, prices } = require('../requests');
  * @param {*} Size Amount in base size, for some reason Coinbase only let you edit orders in the base price (FET)
  */
 const DCA = async (asset, size) => {
-	const info = await prices.getAsset(asset);
-	if (info.error) Promise.reject(info.message);
-	console.log("Starting DCA for " + info.product_id);
+    const info = await prices.getAsset(asset);
+    if (info.error) Promise.reject(info.message);
+    console.log("Starting DCA for " + info.product_id);
 
-	const quoteDigits = info.quote_increment.split('.')[1].length;
-	const quoteIncrement = parseFloat(info.quote_increment);
-	const baseIncrement = info.base_increment.split('.')[1].length;
+    const quoteDigits = info.quote_increment.split('.')[1].length;
+    const quoteIncrement = parseFloat(info.quote_increment);
+    const baseIncrement = info.base_increment.split('.')[1].length;
 
     const IsOrderFilled = async (order_id) => (await order.get(order_id)).order.status === 'FILLED';
     const getLowestBid = async () => (await prices.getBidAsk(asset)).pricebooks.find(price => price.product_id === asset).bids[0].price;
@@ -26,14 +26,14 @@ const DCA = async (asset, size) => {
     const reqPrice = await shouldTryPrice();
     const request = await order.create(asset, 'BUY', ((size / parseFloat(reqPrice)).toFixed(baseIncrement)).toString(), reqPrice);
     if (!request.success) return Promise.reject(request.error_response);
-    
+
     let orderInfo = {
         id: request.success_response.order_id,
         size: request.order_configuration.limit_limit_gtc.base_size,
         price: parseFloat(request.order_configuration.limit_limit_gtc.limit_price),
     };
-	
-	console.log('Order placed: ', orderInfo.id + '\nPrice: ' + orderInfo.price);
+
+    console.log('Order placed: ', orderInfo.id + '\nPrice: ' + orderInfo.price);
 
     while (!(await IsOrderFilled(orderInfo.id))) {
         const newPrice = parseFloat(await shouldTryPrice());
@@ -49,10 +49,10 @@ const DCA = async (asset, size) => {
                 console.log('Edited order to: ', newPrice);
                 orderInfo.price = newPrice;
             } else {
-				if (edit.errors[0]?.edit_failure_reason === 'ORDER_NOT_FOUND') continue; // the order was filled while trying to edit
+                if (edit.errors[0]?.edit_failure_reason === 'ORDER_NOT_FOUND') continue; // the order was filled while trying to edit
                 console.log('Failed order edit', edit);
             }
-			console.log('------------------------------------');
+            console.log('------------------------------------');
         } else {
             console.log('Difference not over threshold ' + difference);
             console.log('------------------------------------');
@@ -60,7 +60,7 @@ const DCA = async (asset, size) => {
 
         await new Promise(resolve => setTimeout(resolve, 2000)); 
     }
-	return Promise.resolve(orderInfo);
+    return Promise.resolve(orderInfo);
 };
 
 const args = process.argv.slice(2);
--- operations/accountInfo.js
+++ operations/accountInfo.js
@@ -9,26 +9,26 @@ const getAccountInformation = async () => {
         available_balance: value
     } = acc.accounts.find(account => account.name === "GBP Wallet");
 
-	const isEmpty = (obj) => Object.keys(obj).length === 0;
-	const getAmounts = data => 
-		data.filter(s => s.status === "completed" && new Date(s.created_at) > start)
-		.reduce((total, s) => total + parseFloat(s.amount?.amount || 0), 0);
+    const isEmpty = (obj) => Object.keys(obj).length === 0;
+    const getAmounts = data => 
+        data.filter(s => s.status === "completed" && new Date(s.created_at) > start)
+        .reduce((total, s) => total + parseFloat(s.amount?.amount || 0), 0);
 
-	const balanceRes = await account.getPortfolio(portfolio_uuid);
-	const depositsRes = await account.getAccountDeposits(account_uuid);
-	const withdrawalsRes = await account.getAccountWithdrawals(account_uuid);
+    const balanceRes = await account.getPortfolio(portfolio_uuid);
+    const depositsRes = await account.getAccountDeposits(account_uuid);
+    const withdrawalsRes = await account.getAccountWithdrawals(account_uuid);
 
-	if (isEmpty(balanceRes) || isEmpty(depositsRes) || isEmpty(withdrawalsRes)) return;
+    if (isEmpty(balanceRes) || isEmpty(depositsRes) || isEmpty(withdrawalsRes)) return;
 
-	const worth = parseFloat(balanceRes.breakdown.portfolio_balances.total_balance.value);
-	const deposits = getAmounts(depositsRes.data);
-	const withdrawals = getAmounts(withdrawalsRes.data);
+    const worth = parseFloat(balanceRes.breakdown.portfolio_balances.total_balance.value);
+    const deposits = getAmounts(depositsRes.data);
+    const withdrawals = getAmounts(withdrawalsRes.data);
 
-	const profit = Math.round(worth - (deposits - withdrawals), 2);
+    const profit = Math.round(worth - (deposits - withdrawals), 2);
 
-	const data = { worth, deposits, withdrawals, profit };
+    const data = { worth, deposits, withdrawals, profit };
 
-	console.log(data)
+    console.log(data)
 };
 
 getAccountInformation();
